22a1b8217cf2889f51d27479ced9d5bd9c2be3f0
maloleg <33466049+maloleg@users.noreply.github.com>
2020-05-29 22:59:34 +0300

    krasivo

--- Matrix.hpp
+++ Matrix.hpp
@@ -22,7 +22,7 @@ void Time_testing(Matrix<T> first, Matrix<T> second, size_t thread1, size_t thre
 
     for (size_t i = thread1; i < thread2 + 1; i++){
         Matrix<T>::SetParallel(i);
-        std::cout << "Testing with " << i << " threads:";
+        std::cout << "Testing with " << i << " threads:\n";
         time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
         result = first * second;
         std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count() - time << "ms\n";
@@ -47,33 +47,15 @@ void Time_testing(Matrix<T> first, Matrix<T> second, size_t thread1){
 
 template<typename T>
 void ElementComputation(Matrix<T> lhs, Matrix<T> rhs, Matrix<T>& result, std::vector<bool>& Done, size_t threadN, uint_fast64_t& LastComputed) {
-    T temp = 0;
+    T temp;
     uint_fast64_t Elements_count = 0;
     uint_fast64_t size = Done.size();
 
 
     while (LastComputed < size){
         temp = 0;
-
-        uint_fast64_t element;
-        m_lock.lock();
-        //LastComputed++;
-        //Done[b] = true;
-//        for (uint_fast64_t i = 0; i < size; i++){
-//            if (Done[i] == false){
-//                element = i;
-//                Done[i] = true;
-//                break;
-//            }
-//        }
-        m_lock.unlock();
-        //std::cout << b;
-        //b++;
-        //std::cout << LastComputed << std::endl;
         for (uint_fast64_t i = 0; i < lhs.cols_get(); i++){
-
             temp += lhs.a_get_elem(((LastComputed)/ rhs.cols_get()) * lhs.cols_get() + i) * rhs.a_get_elem((LastComputed) % rhs.cols_get() + i*rhs.cols_get());
-            //temp += lhs.a_get_elem(((element)/ rhs.cols_get()) * lhs.cols_get() + i) * rhs.a_get_elem((element) % rhs.cols_get() + i*rhs.cols_get());
         }
         m_lock.lock();
         LastComputed++;
@@ -86,7 +68,6 @@ void ElementComputation(Matrix<T> lhs, Matrix<T> rhs, Matrix<T>& result, std::ve
     std::cout << "Thread #" << threadN + 1 << " ended his job. " << Elements_count << " elements was computed\n";
     m_lock.unlock();
 }
-
 
 template <typename T>
 Matrix<T> operator* (const Matrix<T>& lhs, const Matrix<T>& rhs){
@@ -142,52 +123,21 @@ private:
     uint_fast64_t rows, cols;
     std::vector<T> a;
     static size_t parallel;
-    std::vector<bool> Done;
 public:
 
-    Matrix(){
-        a.clear();
-        rows = 0;
-        cols = 0;
-        //parallel = 0;
-    }
+    Matrix();
 
-    uint_fast64_t cols_get(){
-        return this->cols;
-    }
+    uint_fast64_t cols_get();
 
-    uint_fast64_t rows_get(){
-        return this->rows;
-    }
+    T a_get_elem(uint_fast64_t numb);
 
-    T a_get_elem(uint_fast64_t numb){
-        if (numb < a.size()) return a[numb];
-        else return -1;
-    }
+    void a_resize(uint_fast64_t size);
 
-    void a_resize(uint_fast64_t size){
-        a.resize(size);
-    }
+    void rows_set(T data);
 
-    void rows_set(T data){
-        rows = data;
-    }
+    void cols_set(T data);
 
-    void cols_set(T data){
-        cols = data;
-    }
-
-    void a_set_elem(uint_fast64_t numb, T data){
-        if (numb < a.size()) a[numb] = data;
-    }
-
-    friend void Parallels_init();
-
-    Matrix(std::vector<T> new_m, uint_fast64_t new_rows, uint_fast64_t new_cols){
-        a = new_m;
-        rows = new_rows;
-        cols = new_cols;
-    }
+    void a_set_elem(uint_fast64_t numb, T data);
 
     static void SetParallel(size_t num);
 
@@ -203,16 +153,7 @@ public:
 
     void SaveMatrix(const char* filename);
 
-    void GenerateMatrix(uint_fast64_t _rows, uint_fast64_t _cols){
-        srand(1234);
-        this->a.resize(_rows * _cols);
-        this->rows = _rows;
-        this->cols = _cols;
-        for (uint_fast64_t i = 0; i < rows * cols; i++){
-            //std::cout << rand();
-            this->a[i] = rand()%1000;
-        }
-    }
+    void GenerateMatrix(uint_fast64_t _rows, uint_fast64_t _cols, uint_fast64_t range);
 
 
 };
@@ -246,11 +187,6 @@ void Matrix<T>::SetParallel(size_t num) {
     parallel = num;
 }
 
-template <typename T>
-void Parallels_init() {
-    Matrix<T>::SetParallel(0);
-}
-
 template<typename T>
 void Matrix<T>::SaveMatrix(const char *filename) {
     std::ofstream out;
@@ -270,6 +206,55 @@ void Matrix<T>::SaveMatrix(const char *filename) {
 
     out.close();
 
+}
+
+template <typename T>
+void Matrix<T>::GenerateMatrix(uint_fast64_t _rows, uint_fast64_t _cols, uint_fast64_t range) {
+    srand(1234);
+    this->a.resize(_rows * _cols);
+    this->rows = _rows;
+    this->cols = _cols;
+    for (uint_fast64_t i = 0; i < rows * cols; i++){
+        this->a[i] = rand()%range;
+    }
+}
+
+template <typename T>
+void Matrix<T>::cols_set(T data) {
+    cols = data;
+}
+
+template <typename T>
+void Matrix<T>::a_set_elem(uint_fast64_t numb, T data) {
+    if (numb < a.size()) a[numb] = data;
+}
+
+template <typename T>
+void Matrix<T>::rows_set(T data) {
+    rows = data;
+}
+
+template <typename T>
+void Matrix<T>::a_resize(uint_fast64_t size) {
+    a.resize(size);
+}
+
+template <typename T>
+T Matrix<T>::a_get_elem(uint_fast64_t numb) {
+    if (numb < a.size()) return a[numb];
+    else return -1;
+}
+
+template <typename T>
+uint_fast64_t Matrix<T>::cols_get() {
+    return this->cols;
+}
+
+template <typename T>
+Matrix<T>::Matrix() {
+    a.clear();
+    rows = 0;
+    cols = 0;
 }
 
 
--- main.cpp
+++ main.cpp
@@ -10,40 +10,34 @@ int main() {
     Matrix<double> m4;
     Matrix<double> m5;
 
-    //Parallels_init<double>();
-    //std::ifstream file;
-    //file.open("in.txt");
+    std::ifstream file;
+    file.open("in.txt");
 
-    //size_t p;
-    //file >> p;
- //   Matrix<double>::SetParallel(p);
-    //m1.LoadMatrix(file);
-    //m2.LoadMatrix(file);
-    //m1.GenerateMatrix(20, 30);
-    //m2.GenerateMatrix(30, 10);
-    //Matrix<double>::SetParallel(10);
+    size_t p;
+    file >> p;
+    Matrix<double>::SetParallel(p);
+    m1.LoadMatrix(file);
+    m2.LoadMatrix(file);
 
-    //std::cout << m1 << std::endl << m2;
+    m3 = m1 * m2;
 
-    //m3 = m1 * m2;
-
-    //std::cout << m1 << std::endl << m2;
     //std::cout << m1 << std::endl << m2 << std::endl << m3;
 
-    //m3.SaveMatrix("out1.txt");
+    m3.SaveMatrix("out.txt");
 
     //std::cout << m1.rows_get()*m1.cols_get();
 
 
-    m4.GenerateMatrix(100, 200);
-    m5.GenerateMatrix(200, 500);
+    m4.GenerateMatrix(100, 200, 1000);
+    m5.GenerateMatrix(200, 500, 1000);
+
     //Matrix<double>::SetParallel(1);
     //m3 = m4 * m5;
     //m3.SaveMatrix("Out_Big.txt");
     //m4.SaveMatrix("Out_Big2.txt");
     //m5.SaveMatrix("Out_Big3.txt");
 
-    Time_testing(m4, m5, 1, 15);
+    Time_testing(m4, m5, 1, 10);
 
 
 
